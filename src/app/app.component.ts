80179506ec57f4df508da8f65ceb5532ce037ba5
Mahender <mahenderktl750@gmail.com>
2025-03-02 13:31:07 +0530

    Adding standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from './Components/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [ProfileComponent],
 })
 export class AppComponent {
   title = 'Proj_Ng17_Basics';
